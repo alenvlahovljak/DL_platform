16d4ca2be78a2efe92f14533e848710ca91f1b63
Alen Vlahovljak <alenvlahovljak7@gmail.com>
2019-09-16 16:51:59 +0200

    Add Post req to user

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 //Modules config
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose"); 
+
 
 //Mongoose models config
 
 
+//Routes set-up
+const userRoutes = require("./routes/user/user");
+
+
+//Application config
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 //Mongoose config
 let url = process.env.DATABASEURL || "mongodb://localhost:27017/DL_platfom";
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 //Application set-up
 app.set("view engine", "ejs");
 
-//Routes config
-const userRoutes = require("./routes/user/user");
 
-//Serving routes config
+//Routes config
 app.use(userRoutes);
 
-//Application config
+
+
+
+
 
 
 //********   MAIN   **********/
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,14 +1,21 @@
+//Modules config
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+//Mongoose models config
 const User = require("../../models/User");
 
 //********   USER   **********/
 
-//CREATE route
+//NEW route
 router.get("/user/new", (req, res)=>{
     res.render("user/new");
+});
+
+//CREATE route
+router.post("/user", (req, res)=>{
+    res.send(req.body);
 });
 
 //SHOW route
