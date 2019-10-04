cf65806792c8efe44b06a81e899eb4b9e3da8401
Alen Vlahovljak <alenvlahovljak7@gmail.com>
2019-10-04 17:34:29 +0200

    Change to port 8000

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ app.get("/portal", (req, res)=>{
 
 
 //Port listening
-app.listen(3000,()=>{
+app.listen(8000,()=>{
     console.log("DL server has started!");
     console.log("*************************************");
 });
