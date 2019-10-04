5196023938e866a28a37fd77975540ba849ada80
SamirB94 <52965600+SamirB94@users.noreply.github.com>
2019-10-04 21:14:33 +0200

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import RegisterForm from './RegisterForm'
-import history from '../history';
 
 const App = () => {
     return (
-        <div className="ui container">
-            <Router history={history}>
-            <div>
-                <Switch>
-                <RegisterForm />
-                </Switch>
-            </div>  
-            </Router>
+        <div>
+           <RegisterForm />
         </div>
     );
 };
